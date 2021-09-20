511a799ca9be1d394d0f60fa006e5f2a7ea53e8e
ManuelInfante <infantemanuel.mail@gmail.com>
2021-09-20 02:33:56 +0200

    Push esercizio - esercizio terminato

---
+++ js/main.js
@@ -0,0 +1,191 @@
+const icons = [
+    {
+      name: 'cat',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'crow',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'dog',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'dove',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'dragon',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'horse',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'hippo',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'fish',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas',
+    },
+    {
+      name: 'carrot',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas',
+    },
+    {
+      name: 'apple-alt',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas',
+    },
+    {
+      name: 'lemon',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas',
+    },
+    {
+      name: 'pepper-hot',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas',
+    },
+    {
+      name: 'user-astronaut',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas',
+    },
+    {
+      name: 'user-graduate',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas',
+    },
+    {
+      name: 'user-ninja',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas',
+    },
+    {
+      name: 'user-secret',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas',
+    },
+];
+
+
+
+const colors = [
+    'blue',
+    'orange',
+    'purple',
+];
+
+const iconsContainer = document.getElementById('icons');
+
+const coloredArray = colorIcons(icons, colors)
+print(coloredArray, iconsContainer);
+
+const types = getTypes(coloredArray);
+const select = document.getElementById('type');
+printOptions(types, select);
+
+// Al Change mostrare solo le icone selezionate
+
+select.onchange = function(element) {
+    const filtered = filterValues(icons, element.target.value);
+
+    print(filtered, iconsContainer);
+};
+
+
+
+
+// FUNZIONI:
+
+function print(array, conatiner) {
+    conatiner.innerHTML = '';
+
+    array.forEach((element) => {
+        const {name, prefix, color, family} = element;
+    
+        conatiner.innerHTML +=`
+        <div>
+            <i class="${family} ${prefix + name}" style="color: ${color}"></i>
+            <div class="titile">${name.toUpperCase()}</div>
+        </div>
+        `;
+    });
+};
+
+function colorIcons(array, colors) {
+    const types = getTypes(array);
+    
+    const coloredArray = array.map((element) =>{
+        const indexType = types.indexOf(element.type);
+
+        element.color = colors[indexType];
+        // console.log(element)
+        return element;
+    });
+
+    return coloredArray;
+};
+
+function getTypes(array) {
+    const types = [];
+
+    array.forEach((element) =>{
+        if (!types.includes(element.type)){
+            types.push(element.type);
+        }
+    });
+
+    return types;
+};
+
+function printOptions(array, select) {
+    array.forEach((element) => {
+        select.innerHTML += `<option value="${element}">${element}</option>`
+    });
+};
+
+function filterValues(array, type) {
+    const filteredIcons = array.filter((element) => {
+        if (element.type === type){
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    if(type === ''){
+        return array;
+    }
+    
+    return filteredIcons;
+};
